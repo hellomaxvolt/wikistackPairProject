73d8fa39e43439a30143add611e479bc7f31f47f
Max Rivera <hellomauriciorivera@gmail.com>
2020-10-01 14:13:30 -0400

    formatted

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const app = express()
-app.use(morgan("dev"))
+const app = express();
+app.use(morgan("dev"));
 //using dev to tell morgan what to output. Can also use tiny (and others?) to tailor the output from morgan
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 //not using path.join because we dont need to consider the windows/mac relationship with \ and /
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('HELLO WORLD')
-})
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD");
+});
 
 const PORT = 1337;
 
