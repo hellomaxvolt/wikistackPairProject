099191861a4fc871aa6e5827b215a915b7fed07e
kmshoell <34169711+kmshoell@users.noreply.github.com>
2020-10-01 16:07:38 -0400

    part 2

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const main = require("./views/main")
+const { db, Page, User } = require('./models');
+
 
 const app = express();
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
 app.use(morgan("dev"));
 //using dev to tell morgan what to output. Can also use tiny (and others?) to tailor the output from morgan
 
@@ -11,11 +20,18 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("HELLO WORLD");
+  res.send(main(''));
 });
 
 const PORT = 1337;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const init = async () => {
+  await Page.sync();
+  await User.sync();
+  // make sure that you have a PORT constant
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+}
+
+init();
